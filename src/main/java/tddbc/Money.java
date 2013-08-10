783b7cdd28d261096486759972502b0cd845ac0c
kyon_mm <kyon.mm@gmail.com>
2013-08-10 14:28:12 +0900

    green
    --HG--
    branch : 2013_08_10_02_23_49

---
+++ src/main/java/tddbc/Money.java
@@ -0,0 +1,11 @@
+package tddbc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kyon_mm
+ * Date: 2013/08/10
+ * Time: 14:28
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Money {
+}
