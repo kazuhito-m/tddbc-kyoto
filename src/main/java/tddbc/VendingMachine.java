0d5f473297885e94ea69022e7cda9f9be65c7a47
kazuhito_m <kazuhito.sumpic@gmail.com>
2013-08-12 06:58:41 +0900

    テストの追加。redに。

--- src/main/java/tddbc/VendingMachine.java
+++ src/main/java/tddbc/VendingMachine.java
@@ -5,24 +5,26 @@ import java.util.List;
 
 public class VendingMachine {
 
-    private List<Object> changeBox = new ArrayList<Object>();
+	private List<Object> changeBox = new ArrayList<Object>();
 
-    private List<Money> amountBox = new ArrayList<Money>();
+	private List<Money> amountBox = new ArrayList<Money>();
 
-    /**
+	/**
 	 * 総合計額を表示する。
+	 * 
 	 * @return
 	 */
 	public int displayTotalAmount() {
-        int total = 0;
-        for (Money m : amountBox) {
-             total += m.getAmount();
-        }
-       return total;
+		int total = 0;
+		for (Money m : amountBox) {
+			total += m.getAmount();
+		}
+		return total;
 	}
 
 	/**
 	 * つり銭箱を返す。
+	 * 
 	 * @return
 	 */
 	public List<Object> getChangeBox() {
@@ -31,21 +33,21 @@ public class VendingMachine {
 
 	/**
 	 * (お金などの)投入を受ける。
+	 * 
 	 * @param money
 	 */
-    public void receive(Object money){
-        if(money instanceof Money){
-           this.amountBox.add((Money) money);
-        }
-        else{
-            changeBox.add(money);
-        }
-    }
+	public void receive(Object money) {
+		if (money instanceof Money) {
+			this.amountBox.add((Money) money);
+		} else {
+			changeBox.add(money);
+		}
+	}
 
-    /**
-     * 返金する。
-     */
-    public void refund() {
-        changeBox.addAll(amountBox);
-    }
+	/**
+	 * 返金する。
+	 */
+	public void refund() {
+		changeBox.addAll(amountBox);
+	}
 }
--- src/test/java/tddbc/VendingMachineTest.java
+++ src/test/java/tddbc/VendingMachineTest.java
@@ -16,7 +16,7 @@ public class VendingMachineTest {
 	private VendingMachine sut;
 
 	@Before
-	public void setUp(){
+	public void setUp() {
 		sut = new VendingMachine();
 	}
 
@@ -33,71 +33,79 @@ public class VendingMachineTest {
 		// act
 		List<Object> actual = sut.getChangeBox();
 		// assert
-		assertThat(actual.size() , is(0));
+		assertThat(actual.size(), is(0));
 	}
 
 	@Test
 	public void お金以外を入れたらトータル金額に加算されていない() {
-        お金以外を入れたらトータル金額に加算されていない(1, 0);
-        お金以外を入れたらトータル金額に加算されていない(5, 0);
-        お金以外を入れたらトータル金額に加算されていない(5000, 0);
-        お金以外を入れたらトータル金額に加算されていない("ドル", 0);
+		お金以外を入れたらトータル金額に加算されていない(1, 0);
+		お金以外を入れたらトータル金額に加算されていない(5, 0);
+		お金以外を入れたらトータル金額に加算されていない(5000, 0);
+		お金以外を入れたらトータル金額に加算されていない("ドル", 0);
 	}
 
-    public void お金以外を入れたらトータル金額に加算されていない(Object money, int expected){
-        setUp();
-        sut.receive(money);
-        int actual = sut.displayTotalAmount();
-        assertThat(actual, is(expected));
-    }
+	public void お金以外を入れたらトータル金額に加算されていない(Object money, int expected) {
+		setUp();
+		sut.receive(money);
+		int actual = sut.displayTotalAmount();
+		assertThat(actual, is(expected));
+	}
 
-    public <E> List<E>_(E... elelements){
-        List<E> ls = new ArrayList<E>();
-        for(E e : elelements){
-            ls.add(e);
-        }
-        return ls;
-    }
+	public <E> List<E> _(E... elelements) {
+		List<E> ls = new ArrayList<E>();
+		for (E e : elelements) {
+			ls.add(e);
+		}
+		return ls;
+	}
 
 	@Test
 	public void お金以外を入れたらトータル金額につり銭箱にたまる() {
-        お金以外を入れたらトータル金額につり銭箱にたまる(_((Object)5),_((Object)5));
-        お金以外を入れたらトータル金額につり銭箱にたまる(_((Object)5000),_((Object)5000));
+		お金以外を入れたらトータル金額につり銭箱にたまる(_((Object) 5), _((Object) 5));
+		お金以外を入れたらトータル金額につり銭箱にたまる(_((Object) 5000), _((Object) 5000));
 	}
 
-    public void お金以外を入れたらトータル金額につり銭箱にたまる(List<Object> money, List<Object> expectedObject) {
-        setUp();
-        for(Object o : money){
-            sut.receive(o);
-        }
-        List<Object> actual = sut.getChangeBox();
-        assertThat(actual, is(expectedObject));
-    }
+	public void お金以外を入れたらトータル金額につり銭箱にたまる(List<Object> money,
+			List<Object> expectedObject) {
+		setUp();
+		for (Object o : money) {
+			sut.receive(o);
+		}
+		List<Object> actual = sut.getChangeBox();
+		assertThat(actual, is(expectedObject));
+	}
 
-    @Test
-    public void 有効硬貨を投入できる(){
-        sut.receive(_10);
-        int actual = sut.displayTotalAmount();
-        assertThat(actual , is(10));
-    }
+	@Test
+	public void 有効硬貨を投入できる() {
+		sut.receive(_10);
+		int actual = sut.displayTotalAmount();
+		assertThat(actual, is(10));
+	}
 
-    @Test
-    public void 有効硬貨を複数回投入した金額が総計に反映されている(){
-        sut.receive(_10);
-        sut.receive(_50);
-        sut.receive(_100);
-        sut.receive(_500);
-        sut.receive(_1000);
-        int actual = sut.displayTotalAmount();
-        assertThat(actual , is(1660));
-    }
+	@Test
+	public void 有効硬貨を複数回投入した金額が総計に反映されている() {
+		sut.receive(_10);
+		sut.receive(_50);
+		sut.receive(_100);
+		sut.receive(_500);
+		sut.receive(_1000);
+		int actual = sut.displayTotalAmount();
+		assertThat(actual, is(1660));
+	}
 
-    @Test
-    public void 払い戻すと釣り銭箱に入っている(){
-        有効硬貨を複数回投入した金額が総計に反映されている();
-        sut.refund();
-        List<Object> actual = sut.getChangeBox();
-        assertThat(actual.size(), is(5));
-    }
+	@Test
+	public void 払い戻すと釣り銭箱に入っている() {
+		有効硬貨を複数回投入した金額が総計に反映されている();
+		sut.refund();
+		List<Object> actual = sut.getChangeBox();
+		assertThat(actual.size(), is(5));
+	}
+	
+	@Test
+	public void 払い戻した後は投入金額の総計がゼロになっている() {
+		払い戻すと釣り銭箱に入っている();
+		int actual = sut.displayTotalAmount();
+		assertThat(actual , is(0));
+	}
 
 }
