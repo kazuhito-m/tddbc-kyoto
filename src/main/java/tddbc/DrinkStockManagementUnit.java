a218a0935d9844af645eb4750b936515a86f2e08
kazuhito_m <kazuhito.sumpic@gmail.com>
2013-08-15 06:47:22 +0900

    「飲み物の在庫」管理装置クラス　機能追加。green

--- src/main/java/tddbc/DrinkStockManagementUnit.java
+++ src/main/java/tddbc/DrinkStockManagementUnit.java
@@ -12,8 +12,14 @@ public class DrinkStockManagementUnit {
 	/** 飲み物のスロット群。 */
 	private List<DrinkSlot> slots = new ArrayList<DrinkSlot>();
 
+	/** コンストラクタ。 */
+	public DrinkStockManagementUnit() {
+		// 現仕様では、決め打ちで１スロット、中身も限定。
+		DrinkSlot slot = new DrinkSlot();
+		slots.add(slot);
+	}
+	
 	public List<DrinkSlot> getSlots() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return slots;
 	} 
 }
