10becca11a4e4eb084dc96fbd73efa77313f6459
Kazuhito Miura <kazuhito.sumpic@gmail.com>
2013-08-16 08:38:45 +0900

    DrinkStockManagementUnitのリファクタリング。テストのリファクタリング。書式

--- src/test/java/tddbc/DrinkStockManagementUnitTest.java
+++ src/test/java/tddbc/DrinkStockManagementUnitTest.java
@@ -8,10 +8,11 @@ import static org.junit.Assert.assertThat;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
+
+import static tddbc.DrinkKind.*;
 
 /**
  * 「飲み物の在庫」管理装置クラステスト。
@@ -51,7 +52,7 @@ public class DrinkStockManagementUnitTest {
 		@Test
 		public void 指定した種類の在庫があるかを確認できる() {
 			// act
-			boolean actual = sut.existStock(DrinkKind.COLA);
+			boolean actual = sut.existStock(COLA);
 			// assert
 			assertThat(actual, is(true));
 		}
@@ -59,16 +60,16 @@ public class DrinkStockManagementUnitTest {
 		@Test
 		public void 指定した種類の在庫を一つ取り出す() {
 			// act
-			Drink actual = sut.takeOut(DrinkKind.COLA);
+			Drink actual = sut.takeOut(COLA);
 			// assert
 			assertThat(actual, is(notNullValue()));
-			assertThat(actual.getCaption(), is(DrinkKind.COLA.getCaption()));
+			assertThat(actual.getCaption(), is(COLA.getCaption()));
 		}
 
 		@Test
 		public void 指定した種類の在庫を取り出した後一つ減っている() {
 			// act
-			sut.takeOut(DrinkKind.COLA);
+			sut.takeOut(COLA);
 			// assert
 			DrinkSlot slot = sut.getSlots().get(0);
 			assertThat(slot.getStockCount(), is(4));
@@ -85,7 +86,7 @@ public class DrinkStockManagementUnitTest {
 			sut = new DrinkStockManagementUnit();
 			// 要らんくらい、絶対なくなるくらい取り出す。
 			while (true) {
-				if (sut.takeOut(DrinkKind.COLA) == null) {
+				if (sut.takeOut(COLA) == null) {
 					break;
 				}
 			}
@@ -94,7 +95,7 @@ public class DrinkStockManagementUnitTest {
 		@Test
 		public void すべての在庫を取り出し切り在庫がないことを確認する() {
 			// act
-			boolean actual = sut.existStock(DrinkKind.COLA);
+			boolean actual = sut.existStock(COLA);
 			// assert
 			assertThat(actual, is(false));
 		}
@@ -102,10 +103,10 @@ public class DrinkStockManagementUnitTest {
 		@Test
 		public void 在庫が一つも無い状態で取り出すとないことが検知出来て在庫状態も変わらない() {
 			// act
-			Drink actual = sut.takeOut(DrinkKind.COLA);
+			Drink actual = sut.takeOut(COLA);
 			// assert
 			assertThat(actual, is(nullValue()));
-			assertThat(sut.existStock(DrinkKind.COLA), is(false));
+			assertThat(sut.existStock(COLA), is(false));
 		}
 	}
 }
