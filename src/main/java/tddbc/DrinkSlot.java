0726fc704f479c94c652eada3e7da9a155f83685
kazuhito_m <kazuhito.sumpic@gmail.com>
2013-08-19 05:18:49 +0900

    リファクタリング。PMD対策。

--- src/main/java/tddbc/DrinkSlot.java
+++ src/main/java/tddbc/DrinkSlot.java
@@ -3,10 +3,8 @@ package tddbc;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 「飲み物の内部スロット」クラス。
- * @author kazuhito_m
- */
+/** 「飲み物の内部スロット」クラス。
+ * @author kazuhito_m */
 public final class DrinkSlot {
 
     /** 飲み物の種類。 */
@@ -22,56 +20,44 @@ public final class DrinkSlot {
         return kind;
     }
 
-    /**
-     * 補充。
-     * @param drink 飲み物。
-     */
-    public final void add(final Drink drink) {
+    /** 補充。
+     * @param drink 飲み物。 */
+    public void add(final Drink drink) {
         drink.setKind(kind);
         drinks.add(drink);
     }
 
-    /**
-     * スロットの在庫個数。
-     * @return 個数。
-     */
-    public final int getStockCount() {
+    /** スロットの在庫個数。
+     * @return 個数。 */
+    public int getStockCount() {
         return drinks.size();
     }
 
     // プロパティ群
 
-    /**
-     * スロットで扱う飲み物の種類。
-     * @param kind 飲み物の種類。
-     */
-    public final void setKind(final DrinkKind kind) {
+    /** スロットで扱う飲み物の種類。
+     * @param kind 飲み物の種類。 */
+    public void setKind(final DrinkKind kind) {
         this.kind = kind;
     }
 
-    /**
-     * スロットで扱う飲み物の価格を返す。
-     * @return 飲み物の価格整数。
-     */
-    public final int getPrice() {
+    /** スロットで扱う飲み物の価格を返す。
+     * @return 飲み物の価格整数。 */
+    public int getPrice() {
         return price;
     }
 
-    /**
-     * スロットで扱う飲み物の価格を設定する。
-     * @param price 設定を行いたい飲み物の価格。
-     */
-    public final void setPrice(final int price) {
+    /** スロットで扱う飲み物の価格を設定する。
+     * @param price 設定を行いたい飲み物の価格。 */
+    public void setPrice(final int price) {
         this.price = price;
     }
 
-    /**
-     * 飲み物を一個取り出す。<br/>
+    /** 飲み物を一個取り出す。<br/>
      * (外へガシャコン！と落とすイメージ)<br/>
      * 在庫管理法は「後入れ先出し」とする。
-     * @return 飲み物一個。
-     */
-    public final Drink takeOut() {
+     * @return 飲み物一個。 */
+    public Drink takeOut() {
         if (this.drinks.size() == 0) {
             return null;
         } else {
